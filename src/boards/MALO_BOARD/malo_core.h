45fbfd43fa1bdc1f653d8394ee1ca81731ef79fe
parallellogic- <parallellogic@gmail.com>
2026-06-14 22:29:26 -0700

    dual core external flash with py on core0 and c on core1 on rp2350b

---
+++ src/boards/MALO_BOARD/malo_core.h
@@ -0,0 +1,55 @@
+#ifndef _BOARDS_MALO_CORE_COMMON_H
+#define _BOARDS_MALO_CORE_COMMON_H
+
+// --- LED ---
+#ifndef PICO_DEFAULT_LED_PIN
+#define PICO_DEFAULT_LED_PIN 37
+#endif
+
+// --- UART ---
+#ifndef PICO_DEFAULT_UART
+#define PICO_DEFAULT_UART 0
+#endif
+#ifndef PICO_DEFAULT_UART_TX_PIN
+#define PICO_DEFAULT_UART_TX_PIN 0
+#endif
+#ifndef PICO_DEFAULT_UART_RX_PIN
+#define PICO_DEFAULT_UART_RX_PIN 1
+#endif
+
+// --- I2C ---
+#ifndef PICO_DEFAULT_I2C
+#define PICO_DEFAULT_I2C 1
+#endif
+#ifndef PICO_DEFAULT_I2C_SDA_PIN
+#define PICO_DEFAULT_I2C_SDA_PIN 46
+#endif
+#ifndef PICO_DEFAULT_I2C_SCL_PIN
+#define PICO_DEFAULT_I2C_SCL_PIN 47
+#endif
+
+// --- SPI ---
+#ifndef PICO_DEFAULT_SPI
+#define PICO_DEFAULT_SPI 1
+#endif
+#ifndef PICO_DEFAULT_SPI_SCK_PIN
+#define PICO_DEFAULT_SPI_SCK_PIN 10
+#endif
+#ifndef PICO_DEFAULT_SPI_TX_PIN
+#define PICO_DEFAULT_SPI_TX_PIN 11
+#endif
+#ifndef PICO_DEFAULT_SPI_RX_PIN
+#define PICO_DEFAULT_SPI_RX_PIN 8
+#endif
+#ifndef PICO_DEFAULT_SPI_CSN_PIN
+#define PICO_DEFAULT_SPI_CSN_PIN 9
+#endif
+
+// Flash configuration
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+#endif
--- src/boards/MALO_BOARD/mpconfigboard.h
+++ src/boards/MALO_BOARD/mpconfigboard.h
@@ -1,4 +1,13 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "ParallelLogic MalO"
+#define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
 //#define MICROPY_HW_FLASH_STORAGE_BYTES  (3145728) // 3MB filesystem matching your CMake config
 //#define MICROPY_HW_FLASH_ROOT_DIR       (1)       // Tells the boot sequence to initialize from the baked root image
+//#define MICROPY_HW_MCU_NAME   "RP2350B"
+//#define PICO_RP2350A 0
+//#define PICO_RP2350B 1 
+//#define PICO_TYPE    PICO_TYPE_RP2350B
+//#define MICROPY_HW_NUM_PIN_INDEX (48)
+
+// Explicitly declare 15MB filesystem space matching the CMake file
+//#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
