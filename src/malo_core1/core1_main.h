15b6e5014156058d60115d3132d958a2cb3a83bb
parallellogic- <parallellogic@gmail.com>
2026-06-15 20:45:36 -0700

    mixed c and cpp on core1

--- src/malo_core1/core1_main.h
+++ src/malo_core1/core1_main.h
@@ -7,8 +7,16 @@
 //Charlieplex led_lower(0);
 //Charlieplex led_upper(1);
 
-// This function runs exclusively on Core 1
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// This function runs exclusively on Core 1 and is safe for both C and C++ files
 void malo_core1_entry(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 //bool set_charlieplex_led(uint8_t bank_index,uint8_t led_index,uint8_t brightness);
 //bool set_effective_led_count(uint8_t bank_index,uint8_t led_count);
