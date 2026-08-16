2b3d420a441f016abdcbe2bb92b1a508e08de1b3
parallellogic- <parallellogic@gmail.com>
2026-08-15 20:47:12 -0700

    added lv_conf.h

--- doc/user_guide.md
+++ doc/user_guide.md
@@ -35,7 +35,7 @@ The following steps will resolve the above issues (the entire process should tak
 1. Observe the red LED on the back of the device begins flickering erratically for approximately one minute
 1. There are now two folders "animations" and "data" on the MalO SAO
 1. Safely dismount the device from your computer
-1. Power cycle the device by tapping the "Reset" button
+1. Power cycle the device by tapping the "Reboot" button
 1. The MalO SAO has now been factory reset
 
 # Hardware Setup
---
+++ src/experimentation/file_serial8/file_serial8.ino
@@ -0,0 +1,13 @@
+//WAS Adafruit USB with custom call_back functions, IS Pico SDK + FatFS + FatFSUSB
+//arduino ide, rp2350.  use Pico SDK, FatFS, FatFSUSB.  have rp2350 format the section of the flash (FYI, 14 MB for file system, 2 MB for applciation program) as FAT is it is not already (should appear as a mass-storage dvice in Windows/Ubunutu with disk name "MALO").
+//
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
